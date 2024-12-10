406a820eeec0248e40a9a3cdc4ff581944a407a7
kimkihyun <kkhkh4531@gmail.com>
2024-12-10 14:23:40 +0900

    tag, book_tag commit

--- src/main/java/com/nhnacademy/bookapi/entity/BookTag.java
+++ src/main/java/com/nhnacademy/bookapi/entity/BookTag.java
@@ -1,5 +1,15 @@
 package com.nhnacademy.bookapi.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class BookTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Tag tag;
 
 }
