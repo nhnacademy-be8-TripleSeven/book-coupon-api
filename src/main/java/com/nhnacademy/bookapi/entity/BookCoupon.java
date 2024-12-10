03fc760ff160507b933e334bfece6559e26158d9
정회석 <jeonghoe@jeonghoeseog-ui-noteubug.local>
2024-12-10 14:32:25 +0900

    entity

--- src/main/java/com/nhnacademy/bookapi/entity/BookCoupon.java
+++ src/main/java/com/nhnacademy/bookapi/entity/BookCoupon.java
@@ -1,5 +1,28 @@
 package com.nhnacademy.bookapi.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class BookCoupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    @Setter
+    private Book book;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id", nullable = false)
+    @Setter
+    private Coupon coupon;
 
 }
