85d766b2d469e762a9c6469cb6d1305531b435cc
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-11 13:26:51 +0900

    type

--- src/main/java/com/nhnacademy/bookapi/repository/BookCreatorRepository.java
+++ src/main/java/com/nhnacademy/bookapi/repository/BookCreatorRepository.java
@@ -1,8 +1,12 @@
 package com.nhnacademy.bookapi.repository;
 
 import com.nhnacademy.bookapi.entity.BookCreator;
+import com.nhnacademy.bookapi.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookCreatorRepository extends JpaRepository<BookCreator, Long> {
 
+    @Query("select b from BookCreator b where b.name =:name and b.role =:role")
+    BookCreator existByNameAndRole(String name, Role role);
 }
