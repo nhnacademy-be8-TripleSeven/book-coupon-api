159565797a31e50e3583573c97b09b670869ea1d
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-12 21:03:59 +0900

    book detail service Testcord

--- src/main/java/com/nhnacademy/bookapi/service/book/BookService.java
+++ src/main/java/com/nhnacademy/bookapi/service/book/BookService.java
@@ -1,8 +1,16 @@
 package com.nhnacademy.bookapi.service.book;
 
 import com.nhnacademy.bookapi.dto.book.SearchBookDetailDTO;
+import com.nhnacademy.bookapi.entity.Book;
 
 public interface BookService {
+
+    Book save(Book book);
+
+
+    Book update(Book book);
+
+    void delete(Long id);
 
     SearchBookDetailDTO searchBookDetailByBookId(Long id);
 
--- src/main/java/com/nhnacademy/bookapi/service/book/impl/BookServiceImpl.java
+++ src/main/java/com/nhnacademy/bookapi/service/book/impl/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.bookapi.service.book.impl;
 
 import com.nhnacademy.bookapi.dto.book.SearchBookDetailDTO;
+import com.nhnacademy.bookapi.entity.Book;
 import com.nhnacademy.bookapi.entity.BookCreator;
 import com.nhnacademy.bookapi.exception.BookCreatorNotFoundException;
 import com.nhnacademy.bookapi.exception.BookNotFoundException;
@@ -9,6 +10,7 @@ import com.nhnacademy.bookapi.repository.BookRepository;
 import com.nhnacademy.bookapi.service.book.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +19,26 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
     private final BookCreatorRepository bookCreatorRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Book update(Book book) {
+        Book selectBook = bookRepository.findById(book.getId()).orElse(null);
+        if(selectBook == null) {
+            throw new BookNotFoundException("book not found");
+        }
+        BeanUtils.copyProperties(book, selectBook, "id");
+        return bookRepository.save(selectBook);
+    }
+
+    @Override
+    public void delete(Long id) {
+        bookRepository.deleteById(id);
+    }
 
     @Override
     public SearchBookDetailDTO searchBookDetailByBookId(Long id) {
