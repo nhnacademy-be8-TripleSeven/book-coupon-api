9b35d93af5aea6e632af99eef7e631b3231002a8
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-12 09:25:25 +0900

    mysql 적용

--- src/main/java/com/nhnacademy/bookapi/BookapiApplication.java
+++ src/main/java/com/nhnacademy/bookapi/BookapiApplication.java
@@ -1,19 +1,10 @@
 package com.nhnacademy.bookapi;
 
-import com.nhnacademy.bookapi.entity.Book;
-import com.nhnacademy.bookapi.entity.BookTag;
-import com.nhnacademy.bookapi.entity.Tag;
-import com.nhnacademy.bookapi.repository.BookIndexRepository;
-import com.nhnacademy.bookapi.repository.BookRepository;
-import com.nhnacademy.bookapi.repository.BookTagRepository;
-import com.nhnacademy.bookapi.repository.TagRepository;
-import com.nhnacademy.bookapi.service.book_index.BookIndexService;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.context.annotation.Bean;
-import java.time.LocalDate;
+
 
 
 @SpringBootApplication
---
+++ src/main/java/com/nhnacademy/bookapi/service/book/BookService.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.bookapi.service.book;
+
+import java.util.Optional;
+
+public interface BookService {
+
+
+
+}
