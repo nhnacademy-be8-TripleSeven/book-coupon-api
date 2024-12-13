aba5f130fd8e292a9691b877e0e06f619a8ba329
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-13 10:54:37 +0900

    book detail service Testcord

--- src/main/java/com/nhnacademy/bookapi/entity/Book.java
+++ src/main/java/com/nhnacademy/bookapi/entity/Book.java
@@ -14,7 +14,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.elasticsearch.annotations.Document;
+
 
 @Entity
 @NoArgsConstructor
--- src/main/java/com/nhnacademy/bookapi/service/book/BookApiSaveService.java
+++ src/main/java/com/nhnacademy/bookapi/service/book/BookApiSaveService.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.bookapi.service.book;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.nhnacademy.bookapi.elasticsearch.document.BookDocument;
 
 import com.nhnacademy.bookapi.entity.Book;
 import com.nhnacademy.bookapi.entity.BookCategory;
