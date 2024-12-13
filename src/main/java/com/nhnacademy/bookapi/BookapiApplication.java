04843586901abcbe3bd98f69d4bf03df1d9048a7
정회석 <jeonghoe@jeonghoeseog-ui-noteubug.local>
2024-12-13 10:18:35 +0900

    logback

--- src/main/java/com/nhnacademy/bookapi/BookapiApplication.java
+++ src/main/java/com/nhnacademy/bookapi/BookapiApplication.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.bookapi;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,9 +11,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BookapiApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BookapiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BookapiApplication.class, args);
+
+        // 테스트 로그 출력
+        logger.info("INFO 레벨 로그: 애플리케이션이 시작되었습니다.");
+        logger.warn("WARN 레벨 로그: 경고 메시지입니다.");
     }
 
 }
--- src/main/java/com/nhnacademy/bookapi/repository/BookRepository.java
+++ src/main/java/com/nhnacademy/bookapi/repository/BookRepository.java
@@ -6,10 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-    @Query("select new SearcBookDTO(b.title, b.description, b.publishDate, b.regularPrice, b.salePrice, b.isbn13, b.stock, b.page, i.url, b.publisher.name, BookCreator) from Book b join BookImage bi on bi.book.id = b.id join Image i on i.id = bi.image.id ")
-    SearchBookDTO searchBookByTitle(String title);
-
 }
 
 
