7756988249235de5144722ce82fab0b528ecad42
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-18 14:06:22 +0900

    엘라스틱서치 수정

--- src/main/java/com/nhnacademy/bookapi/config/ElasticsearchConfig.java
+++ src/main/java/com/nhnacademy/bookapi/config/ElasticsearchConfig.java
@@ -9,24 +9,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@Configuration
-@EnableElasticsearchRepositories(
-    basePackages = "com.nhnacademy.bookapi.elasticsearch.repository"
-)
-public class ElasticsearchConfig {
-
-    @Bean
-    public RestClient restClient() {
-        return RestClient.builder(new HttpHost("115.94.72.19", 9200)).build();
-    }
-
-    @Bean
-    public RestClientTransport restClientTransport(RestClient restClient) {
-        return new RestClientTransport(restClient, new co.elastic.clients.json.jackson.JacksonJsonpMapper());
-    }
-
-    @Bean
-    public ElasticsearchClient elasticsearchClient(RestClientTransport restClientTransport) {
-        return new ElasticsearchClient(restClientTransport);
-    }
-}
+//@Configuration
+//@EnableElasticsearchRepositories(
+//    basePackages = "com.nhnacademy.bookapi.elasticsearch.repository"
+//)
+//public class ElasticsearchConfig {
+//
+//    @Bean
+//    public RestClient restClient() {
+//        return RestClient.builder(new HttpHost("115.94.72.19", 9200)).build();
+//    }
+//
+//    @Bean
+//    public RestClientTransport restClientTransport(RestClient restClient) {
+//        return new RestClientTransport(restClient, new co.elastic.clients.json.jackson.JacksonJsonpMapper());
+//    }
+//
+//    @Bean
+//    public ElasticsearchClient elasticsearchClient(RestClientTransport restClientTransport) {
+//        return new ElasticsearchClient(restClientTransport);
+//    }
+//}
