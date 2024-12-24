f3335e782262a4708026888565e40536eb315281
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-24 17:20:04 +0900

    fix: entity 필드 예약어 네임수정

--- src/main/java/com/nhnacademy/bookapi/service/book/impl/BookServiceImpl.java
+++ src/main/java/com/nhnacademy/bookapi/service/book/impl/BookServiceImpl.java
@@ -78,7 +78,7 @@ public class BookServiceImpl implements BookService {
         //출판사저장
         String publisher = createBookRequest.getPublisher();
 
-        Publisher existsPublisher = publisherRepository.existsByName(publisher);
+        Publisher existsPublisher = publisherRepository.findByName(publisher);
         if(existsPublisher == null) {
             Publisher newPub = new Publisher(createBookRequest.getPublisher());
             book.publisherUpdate(newPub);
