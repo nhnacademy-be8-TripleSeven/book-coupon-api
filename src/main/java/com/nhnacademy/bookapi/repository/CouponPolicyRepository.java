524b453f29acdc68abfc502c5a4dbef444f49f75
jeonghoe <whrudtnr5@naver.com>
2024-12-26 01:15:48 +0900

    feat(coupon policy): search coupon policy

--- src/main/java/com/nhnacademy/bookapi/repository/CouponPolicyRepository.java
+++ src/main/java/com/nhnacademy/bookapi/repository/CouponPolicyRepository.java
@@ -3,9 +3,12 @@ package com.nhnacademy.bookapi.repository;
 import com.nhnacademy.bookapi.entity.CouponPolicy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
     Optional<CouponPolicy> findById(Long id);
     Optional<CouponPolicy> findByName(String name);
+    List<CouponPolicy> findByNameContainingIgnoreCase(String name);
+
 }
