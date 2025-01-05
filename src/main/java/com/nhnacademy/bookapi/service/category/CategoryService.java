5ed5c4ddfe32058143f2d80d275cff96ebe3cb2f
kimkihyun <kkhkh4531@gmail.com>
2025-01-05 23:49:32 +0900

    fix : 도서 상세 정보 dto 수정

--- src/main/java/com/nhnacademy/bookapi/service/category/CategoryService.java
+++ src/main/java/com/nhnacademy/bookapi/service/category/CategoryService.java
@@ -26,26 +26,4 @@ public interface CategoryService {
     void deleteCategoryById(Long categoryId);
 }
 
-import com.nhnacademy.bookapi.dto.category.CategorySearchDTO;
-import com.nhnacademy.bookapi.entity.Category;
-import com.nhnacademy.bookapi.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-@RequiredArgsConstructor
-public class CategoryService {
-    private final CategoryRepository categoryRepository;
-
-    public List<CategorySearchDTO> searchCategoriesByName(String query) {
-        List<Category> categories = categoryRepository.findByNameContaining(query);
-        return categories.stream()
-                .map(category -> new CategorySearchDTO(category.getId(), category.getName()))
-                .collect(Collectors.toList());
-    }
-
-}
 
