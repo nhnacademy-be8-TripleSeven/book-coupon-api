62cca034cee11adfdd90b927f1e8bf2b0fbebade
Janghyunsung2 <tlswlfk@gmail.com>
2025-01-10 11:45:57 +0900

    fix : 테스트 주석처리

--- src/main/java/com/nhnacademy/bookapi/elasticsearch/repository/CustomBookSearchRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookapi/elasticsearch/repository/CustomBookSearchRepositoryImpl.java
@@ -1,23 +1,10 @@
 package com.nhnacademy.bookapi.elasticsearch.repository;
 
-import static org.apache.commons.lang3.stream.LangCollectors.collect;
 
-import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
-import com.nhnacademy.bookapi.dto.book.BookSearchResponseDTO;
-import com.nhnacademy.bookapi.elasticsearch.document.BookDocument;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
+
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.client.elc.NativeQuery;
-import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.StringQuery;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
